56847ad7d3339eea7b5c248660823e52e2485f65
PedroRibeiro95 <pedro.ribeiro.pr.95@gmail.com>
2017-03-19 18:35:30 +0000

    Still not finished

--- Lab03/src/pt/ist/ap/labs/RunTests.java
+++ Lab03/src/pt/ist/ap/labs/RunTests.java
@@ -1,16 +1,29 @@
 package pt.ist.ap.labs;
 
 import java.lang.reflect.*;
+import javassist.*;
 
 public class RunTests {
    public static void main(String[] args) throws Exception {
-	  Class classTests = Class.forName(args[0]);
       int passed = 0, failed = 0;
-      for (Method m : classTests.getMethods()) {
+      
+      Class classTests = Class.forName(args[0]);
+      
+      for (Method m : classTests.getDeclaredMethods()) {
+    	  //System.out.println("Method: " + m.getName());
          if (m.isAnnotationPresent(Test.class)) {
             try {
                Test test = m.getAnnotation(Test.class);
                String[] setups = test.value();
+               if(setups[0] == "*") {
+            	   for(Method m1 : classTests.getDeclaredMethods()) {
+            		   if (m1.isAnnotationPresent(Setup.class)) {
+            			   m1.invoke(null);
+            		   }
+            	   }
+               }
+               //System.out.println("Method: " + m.getName());
+               //System.out.println("Setups: " + setups[0]);
                invokeSetups(classTests, setups);
                m.invoke(null);
                passed++;
@@ -24,11 +37,11 @@ public class RunTests {
    }
    
    public static void invokeSetups(Class classTests, String[] setups) {
-	   int passed = 0, failed = 0, current = 0;
-	   for(; current < setups.length; current++){
-		   for (Method m : classTests.getMethods()) {
+	   int passed = 0, failed = 0;
+	   for(String current : setups){
+		   for (Method m : classTests.getDeclaredMethods()) {
 		         if (m.isAnnotationPresent(Setup.class) 
-		        		 && m.getAnnotation(Setup.class).value().equals(setups[current])) {
+		        		 && m.getAnnotation(Setup.class).value().equals(current)) {
 		            try {
 		               m.invoke(null);
 		               passed++;
