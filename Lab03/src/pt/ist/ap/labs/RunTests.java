acc16ee664b5ec453666de006de9ce381cecd377
PedroRibeiro95 <pedro.ribeiro.pr.95@gmail.com>
2017-03-19 21:53:19 +0000

    Lab 3 finished, the code is awful tho

--- Lab03/src/pt/ist/ap/labs/RunTests.java
+++ Lab03/src/pt/ist/ap/labs/RunTests.java
@@ -1,29 +1,26 @@
 package pt.ist.ap.labs;
 
 import java.lang.reflect.*;
-import javassist.*;
 
 public class RunTests {
+	
    public static void main(String[] args) throws Exception {
-      int passed = 0, failed = 0;
-      
-      Class classTests = Class.forName(args[0]);
+	  int passed = 0, failed = 0;
+	   
+      Class<?> classTests = Class.forName(args[0]);
       
       for (Method m : classTests.getDeclaredMethods()) {
-    	  //System.out.println("Method: " + m.getName());
+    	 m.setAccessible(true);
          if (m.isAnnotationPresent(Test.class)) {
             try {
                Test test = m.getAnnotation(Test.class);
                String[] setups = test.value();
-               if(setups[0] == "*") {
-            	   for(Method m1 : classTests.getDeclaredMethods()) {
-            		   if (m1.isAnnotationPresent(Setup.class)) {
-            			   m1.invoke(null);
-            		   }
-            	   }
+               if(setups[0].equals("*")) {
+            	   setups = new String[3];
+            	   setups[0] = "s1";
+            	   setups[1] = "s2";
+            	   setups[2] = "s3";
                }
-               //System.out.println("Method: " + m.getName());
-               //System.out.println("Setups: " + setups[0]);
                invokeSetups(classTests, setups);
                m.invoke(null);
                passed++;
@@ -36,19 +33,13 @@ public class RunTests {
       System.out.printf("Passed: %d, Failed %d%n", passed, failed);
    }
    
-   public static void invokeSetups(Class classTests, String[] setups) {
-	   int passed = 0, failed = 0;
+   public static void invokeSetups(Class<?> classTests, String[] setups) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 	   for(String current : setups){
 		   for (Method m : classTests.getDeclaredMethods()) {
+			   	 m.setAccessible(true);
 		         if (m.isAnnotationPresent(Setup.class) 
 		        		 && m.getAnnotation(Setup.class).value().equals(current)) {
-		            try {
-		               m.invoke(null);
-		               passed++;
-		            } catch (Throwable ex) {
-		               System.out.printf("Test %s failed: %s %n", m, ex.getCause());
-		               failed++;
-		            }
+		             m.invoke(null);
 		         }
 		      }
 	   }
