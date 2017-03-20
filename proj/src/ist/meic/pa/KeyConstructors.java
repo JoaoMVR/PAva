4c1afd8ef1fde7833a2cc764f0ead7cb057c6392
Joao Rodrigues <joaovalentimrodrigues95@gmail.com>
2017-03-20 19:25:20 +0000

    first project commit and test files

---
+++ proj/src/ist/meic/pa/KeyConstructors.java
@@ -0,0 +1,17 @@
+package ist.meic.pa;
+
+import java.lang.reflect.*;
+import javassist.*;
+
+public class KeyConstructors {
+	public static void main(String[] args){
+		
+	}
+	
+	public void getClasses(){
+		ClassPool cp = ClassPool.getDefault();
+		for(CtClass ctClass : cp.){
+			
+		}
+	}
+}
---
+++ proj/src/ist/meic/pa/KeywordArgs.java
@@ -0,0 +1,5 @@
+package ist.meic.pa;
+
+public @interface KeywordArgs {
+	String value();
+}
