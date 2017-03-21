a22ecc4dac64b8b0bf9d68fcf215adc95f7ba23a
rodamber <rodrigoambernardo@gmail.com>
2017-03-21 22:50:42 +0000

    New take on template.
    New take on template using reflection. Just an idea.
    
    Also, refactored some things (mostly code organization stuff) and changing
    things like public to private an so on. Check if you agree with everything and
    let me know if you do not.

--- proj/src/ist/meic/pa/KeywordTranslator.java
+++ proj/src/ist/meic/pa/KeywordTranslator.java
@@ -12,25 +12,30 @@ import java.lang.reflect.*;
 
 public class KeywordTranslator implements Translator {
 
+    // FIXME: If it is empty why do we override this?
     @Override
-    public void start(ClassPool arg0) throws NotFoundException, CannotCompileException {
-        //does nothing
+    public void start(ClassPool arg0) throws NotFoundException, 
+                                             CannotCompileException {
+        // Empty.
     }
 
+    /** 
+     *  This is the method that will modify each class in the classpool.
+     */
     @Override
-    public void onLoad(ClassPool pool, String className) throws NotFoundException, CannotCompileException {
-        //this is the method that will modify each class in the classpool
-        CtClass ctClass = pool.get(className);
+    public void onLoad(ClassPool pool, String className) 
+        throws NotFoundException, CannotCompileException {
         try {
-            makeConstructor(ctClass);
+            makeConstructor(pool.get(className));
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public void makeConstructor(CtClass ctClass) throws ClassNotFoundException{
-        CtConstructor[] cons = ctClass.getConstructors();
-        for(CtConstructor other : cons){
+    // FIXME: This should be decoupled from the translator so that we can debug
+    // the generated code by hand.
+    private void makeConstructor(CtClass ctClass) throws ClassNotFoundException{
+        for(CtConstructor other : ctClass.getConstructors()){
             if (other.getAnnotation(KeywordArgs.class) != null){
                 KeywordArgs ann = (KeywordArgs) other.getAnnotation(KeywordArgs.class);
                 treatAnnotations(ctClass, other, ann);
@@ -38,75 +43,67 @@ public class KeywordTranslator implements Translator {
         }
     }
 
-    public void treatAnnotations(CtClass ctClass, CtConstructor ctCons, KeywordArgs annotation){
+    // FIXME: This should be decoupled from the translator so that we can debug
+    // the generated code by hand.
 
-        // IN
-        //     @KeywordArgs("width=100,height=50,margin")
-        //     public Widget(Object... args) {}
+    /**
+     * IN
+     *     @KeywordArgs("width=100,height=50,margin")
+     *     public Widget(Object... args) {}
 
-        // OUT
-        //     public Widget(Object... args) {
-        //       this.width = 100;
-        //       this.height = 50;
+     * OUT
+     *     public Widget(Object... args) {
+     *       this.width = 100;
+     *       this.height = 50;
 
-        //       initialize set with class fields (probably not needed);
-        //       assert args.length is even;
+     *       initialize set with class fields (probably not needed);
+     *       assert args.length is even;
 
-        //       for each (kword, value) in args:
-        //         if field with name kword exists:
-        //           this.'kword = value;
-        //         else:
-        //           throw new RuntimeException("unsuported kword");
-        //     }
+     *       for each (kword, value) in args:
+     *         if field with name kword exists:
+     *           this.'kword = value;
+     *         else:
+     *           throw new RuntimeException("unsuported kword");
+     *     }
+     * Instantiate like: new Widget("height", 10, "width", 2)
+     */
+    private void treatAnnotations(CtClass ctClass, CtConstructor ctCons, 
+                                 KeywordArgs annotation){
+        final String template = makeTemplate(defaultAssignments(annotation));
 
-        // Instantiate like: new Widget("height", 10, "width", 2)
+        // ...
+    }
 
-        // preprocess : String -> [String]
-        // preprocess("width=100,height=50,margin") = ["width=100","height=50"]
-        String[] annFieldNames = preprocess(annotation.value());
+    private String makeTemplate(String[] defaultAssignments) {
         String template = "{";
 
-        for (String name : annFieldNames) {
-            template += "$0." + f + ";";
+        for (String kda : defaultAssignments) {
+            // If kda.equals("height=10") we get "this.height=10;".
+            template += "$0." + kda + ";";
         }
 
-        CtField[] ctFields = ctClass.getFields();
+        template += "for (int i = 0, j = 1; j < $1.length; i += 2, j += 2) {"
+            + "          final String kword = $1[i];"
+            + "          final Object value = $1[j];"
+            + ""
+            + "          try {"
+            + "              Class.forName($0.class).getField(kword).set($0, value);"
+            + "          } catch (NoSuchFieldException e) {"
+            + "              throw new RuntimeException(\"Unrecognized keyword: \" + kword);"
+            + "          }"
+            + "      }";
 
-        template += "for (int i = 0, j = 1; j < $1.length; i += 2, j += 2) {";
-
-        // FIXME: Not yet generating what's supposed to.
-        for (CtField f : ctFields) {
-            template +=
-                "if (\"" + f.getName() + "\".equals($1[i])) {"
-                + "   $0." + f.getName() + " = $1[j];";
-            + "}";
-        }
-
-        /* For the previous example this should generate:
-
-           for (int i = 0, j = 1; j < args.length; i += 2, j += 2) {
-             if ("height".equals(args[i])) {
-               this.height = args[j]; <-- type error
-               continue;
-             }
-             if ("width".equals(args[i])) {
-               this.width = args[j]; <-- type error
-               continue;
-             }
-             if ("margin".equals(args[i])) {
-               this.margin = args[j]; <-- type error
-               continue;
-             }
-             throw new RuntimeException("Unrecognized keyword: " + args[i]);
-           }
-
-        */
-
-        template += "}";
+        return template;
+    }
 
 
-
-
+    /**
+     * defaultAssignments("width=100,height=50,margin") = 
+     *    ["width=100","height=50"]
+     */
+    private String[] defaultAssignments(KeywordArgs annotation) {
+        // FIXME:TODO
+        return null;
     }
 
 }
