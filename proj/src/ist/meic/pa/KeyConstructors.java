de1e87b09f5b85220f98252cb8190512e7ee12a6
rodamber <rodrigoambernardo@gmail.com>
2017-03-26 18:23:32 +0100

    Cleaned KeyConstructors.
    - Added explicit imports.
    - Removed git merge litter.

--- proj/src/ist/meic/pa/KeyConstructors.java
+++ proj/src/ist/meic/pa/KeyConstructors.java
@@ -1,13 +1,10 @@
 package ist.meic.pa;
 
-import javassist.*;
+import javassist.Loader;
+import javassist.ClassPool;
 
 public class KeyConstructors {
-	
-	/*
-	 * TODO: Ask professor about throwing throwable
-	 */
-
+  
     public static void main(String[] args) throws Throwable {
         final int minArgCount = 1;
 
@@ -15,25 +12,12 @@ public class KeyConstructors {
             System.err.println("Usage: java KeyConstructors <class>");
             System.exit(1);
         }
-/*
-<<<<<<< HEAD
-        final Translator translator = new KeywordTranslator();
-        final ClassPool pool        = ClassPool.getDefault();
-        final Loader classLoader    = new Loader();
 
-        classLoader.addTranslator(pool, translator);
-      
-
-        final String[] restArgs = new String[args.length - minArgCount];
-        System.arraycopy(args, minArgCount, restArgs, 0, restArgs.length);
-
-=======*/
         final String[] restArgs = new String[args.length - minArgCount];
         System.arraycopy(args, minArgCount, restArgs, 0, restArgs.length);
 
         final Loader classLoader = new Loader();
         classLoader.addTranslator(ClassPool.getDefault(), new KeywordTranslator());
-// >>>>>>> process-annotations
         classLoader.run(args[0], restArgs);
     }
 
