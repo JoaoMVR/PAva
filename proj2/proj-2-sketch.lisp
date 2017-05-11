d47ef0a9ac2208ab4a4718cd5bade6e4d9191d7c
rodamber <rodrigoambernardo@gmail.com>
2017-05-11 16:39:34 +0100

    Extracted make-constructor

--- proj2/proj-2-sketch.lisp
+++ proj2/proj-2-sketch.lisp
@@ -17,6 +17,33 @@
          (funcall obj ',(intern (concatenate 'string
                                              "SLOT-"
                                              (string x))))))) 
+
+(defun make-constructor (class-name super-classes slots)
+  `(defun ,(intern (concatenate 'string
+                                "MAKE-"
+                                (string class-name))) (&key ,@slots)
+     (lambda (arg)
+       (let ((superclass ,super-classes)
+             (slots
+               ,(cons 'list
+                      (mapcar
+                       #'(lambda (slot)
+                           `(list ',(intern (concatenate 'string
+                                                         "SLOT-"
+                                                         (string slot)))
+                                  ,slot))
+                       slots))))
+         (if (eql arg ',(intern (concatenate 'string
+                                             "CLASS-"
+                                             (string class-name))))
+             t
+             (let ((key-value (assoc arg slots)))
+               (if (car key-value)
+                   (cadr key-value)
+                   (dolist (class ,super-classes)
+                     (let ((res (funcall class arg)))
+                       (when res (return res))))))))))) 
+
 (defmacro def-class (classes &rest slots)
   (let ((class-name (if (listp classes)
                         (car classes)
@@ -24,39 +51,11 @@
         (super-classes (if (listp classes)
                            (cdr classes)
                            nil)))
-  `(progn
+    `(progn
 
-     ;; Constructor
-     (defun ,(intern (concatenate 'string
-                                  "MAKE-"
-                                  (string class-name))) (&key ,@slots)
-       (lambda (arg)
-         (let ((superclass ,super-classes)
-               (slots
-                 ,(cons 'list
-                       (mapcar
-                        #'(lambda (slot)
-                            `(list ',(intern (concatenate 'string
-                                                          "SLOT-"
-                                                          (string slot)))
-                                   ,slot))
-                        slots))))
-           (if (eql arg ',(intern (concatenate 'string
-                                               "CLASS-"
-                                               (string class-name))))
-               t
-              (let ((key-value (assoc arg slots)))
-                  (if (car key-value)
-                      (cadr key-value)
-                      (dolist (class ,super-classes)
-                        (let ((res (funcall class arg)))
-                          (when res (return res))))))))))
-
-     ;; Getters
-     ,@(mapcar (make-getter class-name) slots)
-
-     ;; Recognizer
-     ,(make-recognizer class-name)))) 
+       ,(make-constructor class-name super-classes slots) ; Constructor
+       ,@(mapcar (make-getter class-name) slots)          ; Getters
+       ,(make-recognizer class-name))))                   ; Recognizer
 
 ;;------------------------------------------------------------------------------
 
@@ -65,10 +64,10 @@
 (let ((a (make-person :name "Paulo" :age 33))
       (b "I am not a person"))
   (list
-    (person-name a)
-    (person-age a)
-    (person? a)
-    (person? b))) 
+   (person-name a)
+   (person-age a)
+   (person? a)
+   (person? b))) 
 
 ;;------------------------------------------------------------------------------
 
@@ -79,7 +78,7 @@
 
 (defmacro make-assertions (&body assertions)
   `(progn ,@(mapcar #'(lambda (x) `(assert ,x))
-		    assertions))) 
+                    assertions))) 
 
 (def-class person
   name
@@ -98,31 +97,31 @@
 (def-class (ist-student student sportsman))
 
 (def-class (phd-student ist-student researcher)
-    thesis)
+  thesis)
 
 (let ((s (make-student :name "Paul" :age 21 :course "Informatics")))
   (make-assertions
-   (equal (person-name s) "Paul")
-   (equal (student-course s) "Informatics"))) 
+    (equal (person-name s) "Paul")
+    (equal (student-course s) "Informatics"))) 
 
 (let ((m (make-ist-student :name "Maria" :course "IA" :activity "Tennis")))
   (make-assertions
-   (ist-student? m)
-   (student? m)
-   (sportsman? m)
-   (equal (ist-student-name m) "Maria")
-   (equal (person-name m) "Maria")
-   (equal (sportsman-activity m) "Tennis")
-   (equal (ist-student-activity m) "Tennis")))
+    (ist-student? m)
+    (student? m)
+    (sportsman? m)
+    (equal (ist-student-name m) "Maria")
+    (equal (person-name m) "Maria")
+    (equal (sportsman-activity m) "Tennis")
+    (equal (ist-student-activity m) "Tennis")))
 
 (let ((b (make-phd-student :name "Brian" :age 28 :course "Informatics" :activity "Soccer" :group "ESW" :thesis "Code Migration")))
   (make-assertions
-   (researcher? b)
-   (person? b)
-   (student? b)
-   (sportsman? b)
-   (phd-student? b)
-   (equal (phd-student-thesis b) "Code Migration")
-   (equal (student-name b) "Brian")
-   (equal (phd-student-group b) "ESW")
-   (equal (phd-student-name b) "Brian")))
+    (researcher? b)
+    (person? b)
+    (student? b)
+    (sportsman? b)
+    (phd-student? b)
+    (equal (phd-student-thesis b) "Code Migration")
+    (equal (student-name b) "Brian")
+    (equal (phd-student-group b) "ESW")
+    (equal (phd-student-name b) "Brian")))
