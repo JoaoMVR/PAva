ddc1527a8e5d497ed1e6cf3f935e15de7d20d399
rodamber <rodrigoambernardo@gmail.com>
2017-05-11 20:43:05 +0100

    organized things a little bit

--- proj2/proj-2-sketch.lisp
+++ proj2/proj-2-sketch.lisp
@@ -1,47 +1,50 @@
 (defun make-name (&rest strings)
-  (intern (apply #'concatenate 'string strings))) 
-
-(defun make-recognizer (class-name)
-  `(defun ,(make-name (string class-name) "?") (obj)
-     (typecase obj
-       (function (funcall obj 'RECOGNIZE ',(make-name "CLASS-" (string class-name))))
-       (t nil)))) 
-
-(defun make-getter (class-name)
-  #'(lambda (x)
-      `(defun ,(make-name (string class-name) "-" (string x)) (obj)
-         (funcall obj 'GET-SLOT ',(make-name "SLOT-" (string x)))))) 
-
-(defun make-slots-alist (slots)
-  (cons 'list
-         (mapcar
-          #'(lambda (slot)
-              `(list ',(make-name "SLOT-" (string slot)) ,slot))
-          slots))) 
+  (intern (string-upcase (apply #'concatenate 'string strings))))
 
 (defun delegate (classes query arg)
   (dolist (class classes)
     (let ((res (funcall class query arg)))
-      (when res (return res))))) 
+      (when res (return res)))))
 
 (defun make-constructor (class-name super-classes slots)
   `(defun ,(make-name "MAKE-" (string class-name)) (&key ,@slots)
-     (lambda (query arg)
-       (let ((slots ,(make-slots-alist slots)))
-         (cond
-           ((eql query 'RECOGNIZE) (eql arg ',(intern (concatenate 'string "CLASS-"
-                                                                   (string class-name)))))
-           ((eql query 'GET-SLOT) (let ((key-value (assoc arg slots)))
-                                    (when (car key-value) (cadr key-value))))
-            (t (delegate ,super-classes query arg))))))) 
+     (lambda (query &optional arg)
+       (cond
+         ((eql query 'IS-INSTANCE-OF)
+          (eql arg ',(make-name "CLASS-" (string class-name))))
+
+         ((eql query 'GET-SLOT-VALUE)
+          (let ((key-value (assoc arg ,(make-slots-alist slots))))
+            (when (car key-value) (cadr key-value))))
+
+         (t (delegate ,super-classes query arg))))))
+
+(defun make-getter (class-name)
+  #'(lambda (x)
+      `(defun ,(make-name (string class-name) "-" (string x)) (obj)
+         (funcall obj 'GET-SLOT-VALUE ',(make-name "SLOT-" (string x))))))
+
+(defun make-recognizer (class-name)
+  `(defun ,(make-name (string class-name) "?") (obj)
+     (typecase obj
+       (function (funcall obj 'IS-INSTANCE-OF ',(make-name "CLASS-" (string class-name))))
+       (t nil))))
+
+(defun make-slots-alist (slots)
+  (cons 'list
+        (mapcar
+         #'(lambda (slot)
+             `(list ',(make-name "SLOT-" (string slot)) ,slot))
+         slots)))
 
 (defmacro def-class (classes &rest slots)
-  (let ((class-name    (if (listp classes) (car classes) classes))
-        (super-classes (if (listp classes) (cdr classes) nil)))
+  (let* ((class-name    (if (listp classes) (car classes) classes))
+         (super-classes (when (listp classes) (cdr classes)))
+         (all-slots (append slots ()))) ;(get-all-slots super-classes))))
     `(progn
-       ,(make-constructor class-name super-classes slots) ; Constructor
-       ,@(mapcar (make-getter class-name) slots)          ; Getters
-       ,(make-recognizer class-name))))                   ; Recognizer
+       ,(make-constructor class-name super-classes all-slots) ; Constructor
+       ,@(mapcar (make-getter class-name) all-slots)          ; Getters
+       ,(make-recognizer class-name))))                       ; Recognizer
 
 ;;------------------------------------------------------------------------------
 
@@ -53,28 +56,28 @@
    (person-name a)
    (person-age a)
    (person? a)
-   (person? b))) 
+   (person? b)))
 
 ;;------------------------------------------------------------------------------
 
 ;; We could use this instead to implement the recognizer.
-(gensym) 
+(gensym)
 
 ;;------------------------------------------------------------------------------
 
 (defmacro make-assertions (&body assertions)
   `(progn ,@(mapcar #'(lambda (x) `(assert ,x))
-                    assertions))) 
+                    assertions)))
 
 (def-class person
   name
-  age) 
+  age)
 
 (def-class researcher
-  group) 
+  group)
 
 (def-class (student person)
-  course) 
+  course)
 
 (def-class sportsman
   activity
@@ -88,7 +91,7 @@
 (let ((s (make-student :name "Paul" :age 21 :course "Informatics")))
   (make-assertions
     (equal (person-name s) "Paul")
-    (equal (student-course s) "Informatics"))) 
+    (equal (student-course s) "Informatics")))
 
 (let ((m (make-ist-student :name "Maria" :course "IA" :activity "Tennis")))
   (make-assertions
